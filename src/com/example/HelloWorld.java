2b4981a523760980a48574ef13ce177a2f9c618d
kamakshikhosla <kamakshikhosla3@gmail.com>
2020-01-30 09:40:04 +0530

    filehandling,exception handling codes

--- src/com/example/HelloWorld.java
+++ src/com/example/HelloWorld.java
@@ -1,7 +1,34 @@
 package com.example;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("new branch sample");
+	public void filehandling() throws FileNotFoundException {
+		File file = new File("C:\\Users\\kamakshikhosla\\Desktop\\appiumimp.txt");
+		try {
+			FileInputStream f1 = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void arrayoutexcep() {
+		String ar1 = "VIBGYOR";
+		char ar2[] = ar1.toCharArray();
+		System.out.println("Element at 4th position is::" + ar2[4]);
+		try {
+			System.out.println("Element at 8th position is::" + ar2[8]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Exception thrown is::" + e);
+		}
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// System.out.println("new branch sample");
+		HelloWorld h1 = new HelloWorld();
+		h1.arrayoutexcep();
+		h1.filehandling();
 	}
 }
